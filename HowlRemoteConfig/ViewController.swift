74dcdec0061af09467c5a7b7a9edb01cb8301818
MyeongSic <you6878@icloud.com>
2017-07-14 22:51:15 +0900

    init

--- HowlRemoteConfig/ViewController.swift
+++ HowlRemoteConfig/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    var remoteConfg :FIRRemoteConfig!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        remoteConfg = FIRRemoteConfig.remoteConfig()
+        remoteConfg.setDefaultsFromPlistFileName("FireSwiftRemoteConfigDefaults")
+        
+        
+        fetchConfig()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func fetchConfig(){
+        
+        let remoteConfigsettings = FIRRemoteConfigSettings(developerModeEnabled: true)
+        remoteConfg.configSettings = remoteConfigsettings!
+        
+        remoteConfg.fetch(withExpirationDuration: 0) { (status, error) in
+            
+            
+            if status == .success{
+                self.remoteConfg.activateFetched()
+            }
+            self.displayMessage()
+        }
+        
+        
+        
+    }
+    
+    func displayMessage(){
+        let message = remoteConfg["welcome_message"].stringValue
+        let caps = remoteConfg["welcome_message_caps"].boolValue
+        let color = remoteConfg["backgroundColor"].numberValue
+        
+        
+        if(caps){
+            
+            let alert = UIAlertController(title: "공지사항", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertActionStyle.default, handler: { (action) in
+                exit(0)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        if(color == 1){
+            self.view.backgroundColor = UIColor.black
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
